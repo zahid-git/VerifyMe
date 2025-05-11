15d9dabe1aa63aebab6e3a9e88b5186ea8d3f133
zahid <mdzahidul.islam@wipro.com>
2025-05-12 02:50:02 +0600

    Screen config

--- app/src/main/java/com/verifyme/app/presentation/screens/homepage/HomepageScreen.kt
+++ app/src/main/java/com/verifyme/app/presentation/screens/homepage/HomepageScreen.kt
@@ -1,14 +1,35 @@
 package com.verifyme.app.presentation.screens.homepage
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-
+import com.verifyme.app.presentation.theme.VerifyMeTheme
 
 @Composable
 fun HomepageScreen(
     navController: NavHostController
 ) {
 
+}
 
+
+@Preview
+@Composable
+fun ShowHomePage(){
+
+    VerifyMeTheme {
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White),
+        ) {
+
+        }
+    }
 
 }
